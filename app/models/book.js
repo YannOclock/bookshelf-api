e31a0928fa77254565ab9e7b4a306928f52a0217
Yann Guilloux <yann@oclock.io>
2021-07-23 14:50:52 +0200

    ajout de genre et des auteurs dans la récupération des livres

--- app/models/book.js
+++ app/models/book.js
@@ -2,7 +2,7 @@ const CoreModel = require('./core');
 
 class BookModel extends CoreModel {
 
-    static tableName = 'book';
+    static tableName = 'book_with_genre_and_author';
 
     static fields = [
         "id",
@@ -14,7 +14,6 @@ class BookModel extends CoreModel {
         "language",
         "page_count",
         "cover",
-        "publisher",
         "created_at",
         "updated_at"
     ];
@@ -23,6 +22,9 @@ class BookModel extends CoreModel {
         // execute le constructeur du CoreModel
         super(data);
         this.publisher = `${process.env.API_ENDPOINT}publishers/${data.publisher_id}`;
+        this.genres = data.genre_ids.map(genre_id => `${process.env.API_ENDPOINT}genres/${genre_id}`);
+        this.authors = data.author_ids.map(author_id => `${process.env.API_ENDPOINT}authors/${author_id}`);
+        
     }
 
 }
---
+++ migrations/verify/book_view.sql
@@ -0,0 +1,7 @@
+-- Verify bookshelf:book_view on pg
+
+BEGIN;
+
+SELECT id FROM book_with_genre_and_author WHERE false;
+
+ROLLBACK;
